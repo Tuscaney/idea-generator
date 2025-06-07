39f4fa3d2a64a63044a7531ff70ce0509bbbced5
TutCode222 <tuddycarraway@gmail.com>
2025-06-07 13:48:13 -0500

    Added random ideas in js

---
+++ script.js
@@ -0,0 +1,10 @@
+const ideas = [
+  "Paint something using only 3 colors.",
+  "Write a haiku about your breakfast.",
+  "Take a photo of something ordinary and make it look epic.",
+  "Write a 2-minute monologue for a fictional villain.",
+  "Make a collage from magazine clippings.",
+  "Invent a holiday and design a poster for it.",
+  "Write a short story that starts with a whisper.",
+  "Design an album cover for a band that doesn't exist."
+];
